3c894dba5f1def4f338c8585e168bbf20f874b93
Jesse Piascik <jesse@piascik.net>
2015-05-21 15:03:47 -0500

    Add repos file

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var GitHubApi  = require('github'),
     _          = require('lodash'),
     async      = require('async'),
     prompt     = require('prompt'),
-    git        = require('gift');
+    git        = require('simple-git')(__dirname),
+    fs         = require('fs'),
+    util       = require('util');
 
 var github = new GitHubApi({
     // required
@@ -12,27 +14,33 @@ var github = new GitHubApi({
     }
 });
 
-prompt.message = 'github';
-prompt.start();
-var schema = {
-  properties: {
-    username: {
-      required: true
-    },
-    password: {
-      hidden: true,
-      required: true
+var reposDir = __dirname + "/repos";
+var reposFile = __dirname + "/repos.json";
+
+var authGithub = function(cb) {
+  // Do prompt and go!
+  prompt.message = 'github';
+  prompt.start();
+  var schema = {
+    properties: {
+      username: {
+        required: true
+      },
+      password: {
+        hidden: true,
+        required: true
+      }
     }
-  }
-};
-prompt.get(schema, function (err, result) {
-  github.authenticate({
-      type: "basic",
-      username: result.username,
-      password: result.password
+  };
+  prompt.get(schema, function (err, result) {
+    github.authenticate({
+        type: "basic",
+        username: result.username,
+        password: result.password
+    });
+    cb();
   });
-  go();
-});
+};
 
 var getTopRepos = function(cb) {
   github.search.repos({
@@ -41,14 +49,12 @@ var getTopRepos = function(cb) {
     order: 'desc',
     per_page: 60
   }, function(err, res) {
-    if (err) return cb(err);
-    cb(null,_.map(res.items, function(item) {
-      return _.pick(item,['name', 'full_name', 'watchers']);
-    }));
+    cb(err, res.items);
   });
 };
 
 var getNumTODOs = function(repo, cb) {
+  console.log('Searching for TODO in:', repo);
   github.search.code({
     q: 'q=TODO+repo:' + repo
   }, function(err, res) {
@@ -57,23 +63,77 @@ var getNumTODOs = function(repo, cb) {
   });
 };
 
+var createReposFile = function(cb) {
+  // Make sure our repos dir exists
+  console.log("Checking if " + reposDir + " exists");
+  if (!fs.existsSync(reposDir)){
+      console.log('Creating ', reposDir);
+      fs.mkdirSync(reposDir);
+  }
 
-var go = function() {
-  getTopRepos(function(err, repos) {
-    console.log(JSON.stringify(repos, null, 3));
-    if (err) return console.log('Unable to get Top repos', err);
-    async.mapSeries(repos, function(repo, cb) {
-      setTimeout(function() {
-        getNumTODOs(repo.full_name, function(err, num) {
-          cb(err, _.assign(repo, {todos:num}));
-        });
-      }, 2000);
-    }, function(err, results) {
-      if (err) return console.log(err);
-      results = _.filter(results, function(repo) { return repo.todos > 9; });
-      var sorted = _.sortByOrder(results, ['todos', 'watchers'], [false, true]);
-      console.log(JSON.stringify(sorted, null, 3));
+  authGithub(function() {
+    getTopRepos(function(err, repos) {
+      if (err) return cb(err);
+      async.mapSeries(repos, function(repo, cb) {
+        setTimeout(function() {
+          getNumTODOs(repo.full_name, function(err, num) {
+            cb(err, _.assign(repo, {todos:num}));
+          });
+        }, 2000);
+      }, function(err, results) {
+        if (err) return cb(err);
+        results = _.filter(results, function(repo) { return repo.todos > 9; });
+        var sorted = _.sortByOrder(results, ['todos', 'watchers'], [false, true]);
+        fs.writeFileSync(reposFile, JSON.stringify(sorted, null, 3));
+        cb(null, sorted);
+      });
     });
   });
 };
 
+var clone = function(repo, cb) {
+  console.log('cloning repo:', repo.name);
+  git.clone(repo.clone_url, getRepoPath(repo), cb);
+};
+
+var cloneRepos = function(repos, cb) {
+  async.eachSeries(repos, function(repo, cb) {
+    fs.exists(getRepoPath(repo), function(exists) {
+      if (exists) {
+        console.log("Repo " + repo.name + " already cloned.");
+        return cb(null);
+      }
+      clone(repo, function(err) {
+        if (err) console.log("Error cloning repo for:", repo.name);
+        console.log("Done cloning repo:", repo.name);
+        cb(null);
+      });
+    })
+  }, function(err, results) {
+    cb(err);
+  });
+};
+
+var readReposFile = function(cb) {
+  fs.readFile(reposFile, function (err, data) {
+    if (err) return cb(err);
+    cb(null, JSON.parse(data));
+  });  
+};
+
+var getRepoPath = function(repo) {
+  return reposDir + "/" + repo.name;
+};
+
+if (fs.existsSync(reposFile)) {
+  readReposFile(function(err, repos) {
+    cloneRepos(repos, function(err) {
+      if (err) console.log("Error cloning:", err);
+    });
+  });
+} else {
+  createReposFile(function(err, repos) {
+    if (err) return console.log("Error creating repos.json", err);
+    cloneRepos(repos);
+  });
+}
